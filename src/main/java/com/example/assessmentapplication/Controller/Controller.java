687eb29f81aee6a74c8298e34b08f128a1cd8fdd
43vignesh34 <vigneshmail28105@gmail.com>
2026-02-01 11:36:02 +0530

    Fixing tests while running integration tests

--- src/main/java/com/example/assessmentapplication/Controller/Controller.java
+++ src/main/java/com/example/assessmentapplication/Controller/Controller.java
@@ -5,22 +5,21 @@ import com.example.assessmentapplication.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
+
 @RestController
-public class Controller
-{
+public class Controller {
     @Autowired
     private UserService userService;
 
     @GetMapping("/getUser")
-    public User getUser(@RequestParam("username")String username)
-    {
+    public User getUser(@RequestParam("username") String username) {
         System.out.println(username);
         return userService.findByUsername(username);
     }
 
     @PostMapping("/register")
-    public void registerUser(@RequestBody User user)
-    {
+    public void registerUser(@RequestBody @Valid User user) {
         userService.registerUser(user);
     }
 }
