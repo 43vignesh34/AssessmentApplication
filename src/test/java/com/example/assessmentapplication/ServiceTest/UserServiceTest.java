4d1ecc6186a79099962ca075c14347773f5125af
43vignesh34 <vigneshmail28105@gmail.com>
2026-03-11 22:35:58 +0530

    feat: Added first 2 createAdmin unit tests for UserService's createAdmin method and include a JUnit 5/Mockito unit testing cheat sheet.

---
+++ src/test/java/com/example/assessmentapplication/ServiceTest/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.assessmentapplication.ServiceTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.assessmentapplication.Repository.UserRepository;
+import com.example.assessmentapplication.Service.UserService;
+import com.example.assessmentapplication.entity.User;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    void createAdmin_WhenNoUsersExist() {
+        // Arrange: userRepository.count() returns a 'long', so we must return '0L', not
+        // '0' (int). count() isnt called yet, we prepare for it
+        when(userRepository.count()).thenReturn(0L);
+
+        // Act: Call the real service method
+        userService.createAdmin();
+
+        // Assert: Verify that the save method was called exactly 1 time.
+        // We use any(User.class) to match any User object that was created internally
+        // by the method.
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void createAdmin_WhenUsersExist() {
+        // Arrange: Simulate that 5 users already exist in the database
+        when(userRepository.count()).thenReturn(5L);
+
+        // Act: Call the real service method
+        userService.createAdmin();
+
+        // Assert: Verify that the save method was NEVER called.
+        // never() is the exact same thing as times(0).
+        verify(userRepository, never()).save(any(User.class));
+    }
+}
